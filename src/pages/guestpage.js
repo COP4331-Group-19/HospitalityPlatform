5f6083a1448ff94e10d92d218bcdd180e336648e
avgalbo <54754311+avgalbo@users.noreply.github.com>
2021-07-10 11:09:05 -0400

    Add files via upload

---
+++ src/pages/guestpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Guest from '../components/GuestPage';
+import ScrollToTop from '../components/ScrollToTop';
+
+function GuestPage() {
+  return (
+    <>
+      <ScrollToTop/>
+      <Guest/>
+    </>
+  );
+}
+
+export default GuestPage;
