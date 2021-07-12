7845165521257743ec7643cf6bf73f5e22e609b8
aygarcia <65561606+aygarcia@users.noreply.github.com>
2021-07-12 09:47:06 -0400

    Update index.js

--- src/components/Employee/index.js
+++ src/components/Employee/index.js
@@ -1,27 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import NavbarEmployee from "../Navbar/NavbarEmployee";
+import Icon1 from "../../images/svg-1.svg";
+import Icon2 from "../../images/svg-3.svg";
+import Icon3 from "../../images/svg-5.svg";
+// import Table from "..react-bootstrap/Table";
+// import Card from "../../../react-bootstrap/Cards";
+// import CardGroup from "../react-bootstrap/CardGroup";
+
 import {
-  Container,
+  EmployeeContainer,
+  EmployeeH1,
+  EmployeeWrapper,
+  EmployeeCard,
+  EmployeeIcon,
+  EmployeeH2,
+  EmployeeH3,
+  EmployeeP,
   FormWrap,
-  Icon,
   FormContent,
   Form,
-  FormH1,
-  FormLabel,
-  FormInput,
-  FormButton,
-  Text,
+  FormLittle,
 } from "./EmployeeElements";
 
 const Employee = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <>
-      <Container>
-        <FormWrap>
-          <Icon to="/">Hotel Knightro</Icon>
-          <FormContent></FormContent>
-        </FormWrap>
-      </Container>
-    </>
+    <EmployeeContainer id="Employee">
+      <FormWrap>
+        <FormContent>
+          <Form action="#">
+            <FormLittle action="#">
+              <EmployeeH3>221B</EmployeeH3>
+            </FormLittle>
+          </Form>
+        </FormContent>
+      </FormWrap>
+      <EmployeeH1>Our Employee</EmployeeH1>
+      <EmployeeWrapper>
+        <EmployeeCard>
+          {/* <EmployeeIcon src={Icon1} /> */}
+          <EmployeeH2>Example1</EmployeeH2>
+          <EmployeeP>Example1</EmployeeP>
+        </EmployeeCard>
+        <EmployeeCard>
+          {/* <EmployeeIcon src={Icon2} /> */}
+          <EmployeeH2>Example2</EmployeeH2>
+          <EmployeeP>Example2</EmployeeP>
+        </EmployeeCard>
+        <EmployeeCard>
+          {/* <EmployeeIcon src={Icon3} /> */}
+          <EmployeeH2>Example3</EmployeeH2>
+          <EmployeeP>Example3</EmployeeP>
+        </EmployeeCard>
+      </EmployeeWrapper>
+    </EmployeeContainer>
   );
 };
 
