3929c0f2953a4d2a99ebd3d2f549ed6dce18db3b
aygarcia <65561606+aygarcia@users.noreply.github.com>
2021-07-12 15:30:16 -0400

    Update employee.js

--- src/pages/employee.js
+++ src/pages/employee.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Employee from "../components/Employee";
 import ScrollToTop from "../components/ScrollToTop";
+import NavbarEmployee from "../components/Navbar/NavbarEmployee";
+import Footer from "../components/Footer";
 
 function EmployeePage() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
+      <NavbarEmployee toggle={toggle} />
       <ScrollToTop />
       <Employee />
+      <Footer />
     </>
   );
 }
