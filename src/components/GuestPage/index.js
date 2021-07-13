4982ef5244b3b6c04fa98453a9f3e2f8012522e2
avgalbo <54754311+avgalbo@users.noreply.github.com>
2021-07-13 18:43:32 -0400

    Update index.js

--- src/components/GuestPage/index.js
+++ src/components/GuestPage/index.js
@@ -1,39 +1,86 @@
 import React from "react";
-import Icon1 from "../../images/svg-1.svg";
-import Icon2 from "../../images/svg-3.svg";
-import Icon3 from "../../images/svg-5.svg";
+import GuestAccountSettingComponent from "./GuestAccountSettingComponent";
+import GuestCardComponent from "./GuestCardComponent";
+import NavbarGuest from "../Navbar/NavbarGuest";
 import {
+  AccountSettingWrapper,
   GuestContainer,
   GuestH1,
+  GuestPageHeader,
   GuestWrapper,
   GuestCard,
   GuestIcon,
   GuestH2,
+  GuestH3,
   GuestP,
+  FormWrap,
+  FormContent,
+  Form,
+  FormLittle,
+  Button,
+  FormButton,
+  IncDecButton,
+  Value,
 } from "./GuestElements";
 
 const Guest = () => {
   return (
-    <GuestContainer id="Guest">
-      <GuestH1>Welcome Guest</GuestH1>
-      <GuestWrapper>
-        <GuestCard>
-          {/* <GuestIcon src={Icon1} /> */}
-          <GuestH2>Pillows</GuestH2>
-          <GuestP>Click Here</GuestP>
-        </GuestCard>
-        <GuestCard>
-          {/* <GuestIcon src={Icon2} /> */}
-          <GuestH2>Blankets</GuestH2>
-          <GuestP>Click Here</GuestP>
-        </GuestCard>
-        <GuestCard>
-          {/* <GuestIcon src={Icon3} /> */}
-          <GuestH2>Linens</GuestH2>
-          <GuestP>Click Here</GuestP>
-        </GuestCard>
-      </GuestWrapper>
-    </GuestContainer>
+    <>
+      <GuestContainer>
+      <GuestPageHeader>Guest Account</GuestPageHeader>
+        <FormWrap>
+        
+          <FormContent>
+          
+            <Form>
+              
+              <GuestH3>hello</GuestH3>
+              <FormLittle action="#">
+                <GuestH3>Room 221B</GuestH3>
+              </FormLittle>
+              
+            </Form>
+            
+          </FormContent>
+        </FormWrap>
+         <GuestH1>Order Service</GuestH1>
+         
+         
+        <GuestWrapper>
+          
+          
+          <GuestCardComponent
+            items={"Blankets"}
+            description={"Need more blankets? we'll get you some!"}
+          />
+          <GuestCardComponent 
+          items={"Pillows"}
+          description={"To keep your head supported"}/>
+          <GuestCardComponent />
+          <GuestCardComponent />
+          <GuestCardComponent />
+          <GuestCardComponent />
+          
+        </GuestWrapper>
+        <GuestH1>Account Settings</GuestH1>
+        {/* <Link to="/signin">Edit</Link> */}
+        <AccountSettingWrapper>
+          <GuestAccountSettingComponent
+            settingTitle="FirstName"
+            description="this is a description"
+          />
+          <GuestAccountSettingComponent settingTitle="LastName" />
+          <GuestAccountSettingComponent settingTitle="PhoneNumber" />
+          <GuestAccountSettingComponent settingTitle="Email" />
+          <GuestAccountSettingComponent settingTitle="Username" />
+          <GuestAccountSettingComponent settingTitle="Password" />
+        </AccountSettingWrapper>
+        {/*Setting container*/}
+        {/* AccountSettingComponent settingTitle="FirstName" value="Angel*/}
+        {/* AccountSettingComponent settingTitle="Password hidden={true}*/}
+         </GuestContainer>
+      
+    </>
   );
 };
 
