22088113ab18af0be09a6b3aeda65ca2959b176d
aygarcia <65561606+aygarcia@users.noreply.github.com>
2021-07-13 00:56:02 -0400

    Update AdminElements.js

--- src/components/Admin/AdminElements.js
+++ src/components/Admin/AdminElements.js
@@ -1,68 +1,149 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-// import { Link as LinkR } from "react-router-dom";
 
-// export const Navbar = styled.nav`
-//   background: #080f0f;
-//   height: 80px;
-//   margin-top: -80px;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   font-size: 1rem;
-//   position: sticky;
-//   top: 0;
-//   z-index: 10;
+export const AccountSettingCard = styled.div`
+  background: #2c2c2c;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: 10px;
+  max-height: 340px;
+  padding: 30px;
+  transition: all 0.2s ease-in-out;
 
-//   @media screen and (max-width: 960px) {
-//     transition: 0.8s all ease;
-//   }
-// `;
-// export const NavbarContainer = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   height: 80px;
-//   z-index: 1;
-//   width: 100%;
-//   padding: 0 24px;
-//   max-width: 1100px;
-// `;
-// export const NavLogo = styled(LinkR)`
-//   color: #fff;
-//   justify-self: flex-start;
-//   cursor: pointer;
-//   font-size: 1.5rem;
-//   display: flex;
-//   align-items: center;
-//   margin-left: 24px;
-//   font-weight: bold;
-//   text-decoration: none;
-// `;
-export const Container = styled.div`
-  min-height: 692px;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  top: 0;
-  z-index: 0;
-  overflow: hidden;
-  background: #343434;
-  );
+  &:hover {
+    transform: scale(1.02);
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+  }
+`;
+export const AccountSettingWrapper = styled.div`
+  max-width: 1000px;
+  margin: 100px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  grid-gap: 200px;
+  padding: 0 50px;
+
+  @media screen and (max-width: 1000px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+    padding: 0 20px;
+  }
+`;
+export const AccountSettingH2 = styled.h2`
+  font-size: 1.5rem;
+  color: #6d7275;
+  margin-bottom: 10px;
+`;
+export const AccountP = styled.p`
+  font-size: 1rem;
+  color: white;
+  text-align: center;
 `;
 
-export const FormWrap = styled.div`
-  height: 5%;
+export const AdminContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+  background: #2c2c2c;
 
-  @media screen and (max-width: 400px) {
-    height: 80%;
+  @media screen and (max-width: 768px) {
+    height: 1100px;
+  }
+
+  @media screen and (max-width: 480px) {
+    height: 1300px;
   }
 `;
 
-export const FormWrapTwo = styled.div`
+export const AdminWrapper = styled.div`
+  max-width: 1000px;
+  margin: 100px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  grid-gap: 16px;
+  padding: 0 50px;
+
+  @media screen and (max-width: 1000px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+    padding: 0 20px;
+  }
+`;
+
+export const AdminCard = styled.div`
+  background: #3d3d3d;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: 10px;
+  max-height: 340px;
+  padding: 30px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.02);
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+  }
+`;
+
+export const AdminIcon = styled.img`
+  height: 160px;
+  width: 160px;
+  margin-bottom: 10px;
+`;
+
+export const AdminH1 = styled.h1`
+  font-size: 2.5rem;
+  color: #fff;
+  margin-top: 30px;
+  margin-bottom: -60px;
+  display: flex;
+  justify-content: center;
+  align-items: left;
+
+  @media screen and (max-width: 480px) {
+    font-size: 2rem;
+  }
+`;
+
+export const AdminH2 = styled.h2`
+  font-size: 1rem;
+  color: white;
+  margin-bottom: 10px;
+`;
+export const AdminH3 = styled.h3`
+  font-size: 1.5rem;
+  color: white;
+  margin-bottom: 10px;
+  margin-left: -25px;
+  display: flex;
+  justify-content: center;
+  align-items: left;
+
+  @media screen and (max-width: 480px) {
+    font-size: 2rem;
+  }
+`;
+export const AdminP = styled.p`
+  font-size: 1rem;
+  color: white;
+  text-align: center;
+`;
+export const FormWrap = styled.div`
   height: 90%;
   display: flex;
   flex-direction: column;
@@ -70,30 +151,6 @@ export const FormWrapTwo = styled.div`
 
   @media screen and (max-width: 400px) {
     height: 80%;
-  }
-`;
-export const FormWrapThree = styled.div`
-  height: 10%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  @media screen and (max-width: 400px) {
-    height: 80%;
-  }
-`;
-
-export const Icon = styled(Link)`
-  margin-left: 32px;
-  margin-top: 32px;
-  text-decoration: none;
-  color: #fff;
-  font-weight: 700;
-  font-size: 32px;
-
-  @media screen and (max-width: 480px) {
-    margin-left: 16px;
-    margin-top: 8px;
   }
 `;
 
@@ -107,16 +164,15 @@ export const FormContent = styled.div`
     padding: 10px;
   }
 `;
-
 export const Form = styled.form`
-  background: #474747;
+  background: #3d3d3d;
   max-width: 500px;
   width: 100%;
   z-index: 1;
   display: grid;
   margin: 0 auto;
-  margin-top: -500px;
-  padding: 80px 32px;
+  margin-top: 150px;
+  padding: 50px 250px;
   border-radius: 4px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.9);
 
@@ -124,67 +180,19 @@ export const Form = styled.form`
     padding: 32px 32px;
   }
 `;
-export const FormTwo = styled.form`
-  background: #474747;
+export const FormLittle = styled.form`
+  background: #14cca4;
   max-width: 500px;
-  width: 100%;
-  z-index: 1;
+  width: 50%;
+  z-index: 2;
   display: grid;
-  margin: 0 auto;
-  margin-top: -400px;
-  padding: 80px 32px;
+  margin-top: -30px;
+  margin-left: -190px;
+  padding: 20px 50px;
   border-radius: 4px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.9);
 
   @media screen and (max-width: 400px) {
     padding: 32px 32px;
   }
-`;
-
-export const FormH1 = styled.h1`
-  margin-bottom: 40px;
-  color: #fff;
-  font-size: 20px;
-  font-weight: 400;
-  text-align: center;
-`;
-export const FormH2 = styled.h2`
-  margin-bottom: 40px;
-  color: #fff;
-  font-size: 20px;
-  font-weight: 400;
-  text-align: left;
-`;
-export const FormLabel = styled.label`
-  margin-bottom: 8px;
-  font-size: 14px;
-  color: #fff;
-`;
-export const FormInput = styled.input`
-  padding: 16px 16px;
-  margin-bottom: 32px;
-  border: none;
-  border-radius: 4px;
-`;
-
-export const FormButton = styled.button`
-  background: #c20114;
-  padding: 16px 0;
-  border: none;
-  border-radius: 4px;
-  color: #fff;
-  font-size: 20px;
-  cursor: pointer;
-
-  &:hover {
-    transition: all 0.2s ease-in-out;
-    background: #fff;
-    color: #c20114;
-  }
-`;
-export const Text = styled.span`
-  text-align: center;
-  margin-top: 24px;
-  color: #fff;
-  font-size: 14px;
 `;
