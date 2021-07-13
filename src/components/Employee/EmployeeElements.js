f2f356f9ecf85d4d30d6aeff6c4e2dd3b3751be2
aygarcia <65561606+aygarcia@users.noreply.github.com>
2021-07-12 23:45:25 -0400

    Update EmployeeElements.js

--- src/components/Employee/EmployeeElements.js
+++ src/components/Employee/EmployeeElements.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
+export const AccountSettingCard = styled.div`
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  border-radius: 10px;
+  max-height: 340px;
+  padding: 30px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.02);
+    transition: all 0.2s ease-in-out;
+    cursor: pointer;
+  }
+`;
 export const EmployeeContainer = styled.div`
   height: 1000px;
   display: flex;
