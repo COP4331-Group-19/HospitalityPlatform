aee568f687d0a1706e48a082e0fbff4fee2c1ebc
aygarcia <65561606+aygarcia@users.noreply.github.com>
2021-07-14 12:38:24 -0400

    Update Path.js

--- src/Path.js
+++ src/Path.js
@@ -1,1 +1,15 @@
 
+const app_name = 'cop4331-10'
+exports.buildPath = 
+function buildPath(route)
+{
+    if (process.env.NODE_ENV === 'production') 
+    {
+        return 'https://' + app_name +  '.herokuapp.com/' + route;
+    }
+    else
+    {        
+        return 'http://localhost:5000/' + route;
+    }
+}
+
