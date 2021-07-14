cb1c33deadc21dc171cb8c87160df1428f746a67
avgalbo <54754311+avgalbo@users.noreply.github.com>
2021-07-14 12:44:34 -0400

    Update index.js

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   Container,
   FormWrap,
@@ -13,20 +14,86 @@ import {
 } from "./SigninElements";
 
 const SignIn = () => {
+  const [message, setMessage] = useState("");
+
+  // doLogin just a login function
+  const doLogin = async (event) => {
+    event.preventDefault();
+
+    //JSON OBJECT
+    var obj = { username: LoginName.value, password: LoginPassword.value };
+    var js = JSON.stringify(obj);
+
+    //Making a Payload
+    var config = {
+      method: "post",
+      url: bp.buildPath("api/account/login"),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: js,
+    };
+
+    //Sending Payload to the server
+    axios(config)
+      .then(function (response) {
+        var res = response.data;
+
+        if (res.err_Code) {
+          //Error Message
+          setMessage(res.description);
+        } else {
+          //Import Jasonwebtoken
+          var jwt = require("jsonwebtoken");
+
+          //Removing Bearer
+          var decoded = res.token.replace("Bearer ", "");
+
+          //Decoding the jwt to get the user data
+          var ud = jwt.decode(decoded);
+
+          //Taking out of the payload
+          var userId = ud.id;
+          var UserName = ud.username;
+          var Role = ud.role;
+
+          //Puting stuff in to the user
+          var user = { UserName: UserName, Role: Role, userId: userId };
+
+          //Locally storing user Data
+          localStorage.setItem("user_data", JSON.stringify(user));
+
+          //Go to the user Window
+          window.location.href = "/user";
+        }
+      })
+      .catch(function (error) {
+        //Error function to show error as consol logs
+        setMessage(" " + error);
+      });
+  };
+
   return (
     <>
       <Container>
         <FormWrap>
-          <Icon to="/">Hotel Knightro</Icon>
+          <Icon to="/">CyberHotel</Icon>
           <FormContent>
             <Form action="#">
               <FormH1>Sign in to your account</FormH1>
-              <FormLabel htmlFor="for">Email</FormLabel>
+              <FormLabel htmlFor="for">Username</FormLabel>
               <FormInput type="email" required />
               <FormLabel htmlFor="for">Password</FormLabel>
               <FormInput type="password" required />
-              <FormButton type="submit">Continue</FormButton>
-              <Text>Forgot password</Text>
+
+              <FormLabel> {message} </FormLabel>
+
+              <FormButton type="submit" class="button" onClick={doLogin}>
+                Continue
+              </FormButton>
+              <p classname="forgot-password text-right">
+                <Link to={"/resetpassword"}>Forgot password?</Link>
+              </p>
             </Form>
           </FormContent>
         </FormWrap>
@@ -36,3 +103,4 @@ const SignIn = () => {
 };
 
 export default SignIn;
+
