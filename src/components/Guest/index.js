5d16741d5a60ef625a505a403cd27272041434af
avgalbo <54754311+avgalbo@users.noreply.github.com>
2021-07-14 19:38:35 -0400

    Add files via upload

---
+++ src/components/Guest/index.js
@@ -0,0 +1,87 @@
+import React from "react";
+import GuestAccountSettingComponent from "./GuestAccountSettingComponent";
+import GuestCardComponent from "./GuestCardComponent";
+import NavbarGuest from "../Navbar/NavbarGuest";
+import {
+  AccountSettingWrapper,
+  GuestContainer,
+  GuestH1,
+  GuestPageHeader,
+  GuestWrapper,
+  GuestCard,
+  GuestIcon,
+  GuestH2,
+  GuestH3,
+  GuestP,
+  FormWrap,
+  FormContent,
+  Form,
+  FormLittle,
+  Button,
+  FormButton,
+  IncDecButton,
+  Value,
+} from "./GuestElements";
+
+const Guest = () => {
+  return (
+    <>
+      <GuestContainer>
+      <GuestPageHeader>Guest Account</GuestPageHeader>
+        <FormWrap>
+        
+          <FormContent>
+          
+            <Form>
+              
+              <GuestH3>hello</GuestH3>
+              <FormLittle action="#">
+                <GuestH3>Room 221B</GuestH3>
+              </FormLittle>
+              
+            </Form>
+            
+          </FormContent>
+        </FormWrap>
+         <GuestH1>Order Service</GuestH1>
+         
+         
+        <GuestWrapper>
+          
+          
+          <GuestCardComponent
+            items={"Blankets"}
+            description={"Need more blankets? we'll get you some!"}
+          />
+          <GuestCardComponent 
+          items={"Pillows"}
+          description={"To keep your head supported"}/>
+          <GuestCardComponent />
+          <GuestCardComponent />
+          <GuestCardComponent />
+          <GuestCardComponent />
+          
+        </GuestWrapper>
+        <GuestH1>Account Settings</GuestH1>
+        {/* <Link to="/signin">Edit</Link> */}
+        <AccountSettingWrapper>
+          <GuestAccountSettingComponent
+            settingTitle="FirstName"
+            description="this is a description"
+          />
+          <GuestAccountSettingComponent settingTitle="LastName" />
+          <GuestAccountSettingComponent settingTitle="PhoneNumber" />
+          <GuestAccountSettingComponent settingTitle="Email" />
+          <GuestAccountSettingComponent settingTitle="Username" />
+          <GuestAccountSettingComponent settingTitle="Password" />
+        </AccountSettingWrapper>
+        {/*Setting container*/}
+        {/* AccountSettingComponent settingTitle="FirstName" value="Angel*/}
+        {/* AccountSettingComponent settingTitle="Password hidden={true}*/}
+         </GuestContainer>
+      
+    </>
+  );
+};
+
+export default Guest;
