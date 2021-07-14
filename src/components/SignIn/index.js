5f001032326ee63740c7ad3cb12b7f691a4d0084
avgalbo <54754311+avgalbo@users.noreply.github.com>
2021-07-14 19:36:52 -0400

    Update index.js

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -15,71 +15,71 @@ import {
 } from "./SigninElements";
 
 const SignIn = () => {
-  const [message, setMessage] = useState("");
+//   const [message, setMessage] = useState("");
 
-  //required files
-  var bp = require("../Path.js");
+//   //required files
+//   var bp = require("../Path.js");
 
-  //variables used to login
-  var LoginName;
-  var LoginPassword;
+//   //variables used to login
+//   var LoginName;
+//   var LoginPassword;
 
-  // doLogin just a login function
-  const doLogin = async (event) => {
-    event.preventDefault();
+//   // doLogin just a login function
+//   const doLogin = async (event) => {
+//     event.preventDefault();
 
-    //JSON OBJECT
-    var obj = { username: LoginName.value, password: LoginPassword.value };
-    var js = JSON.stringify(obj);
+//     //JSON OBJECT
+//     var obj = { username: LoginName.value, password: LoginPassword.value };
+//     var js = JSON.stringify(obj);
 
-    //Making a Payload
-    var config = {
-      method: "post",
-      url: bp.buildPath("api/account/login"),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: js,
-    };
+//     //Making a Payload
+//     var config = {
+//       method: "post",
+//       url: bp.buildPath("api/account/login"),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       data: js,
+//     };
 
-    //Sending Payload to the server
-    axios(config)
-      .then(function (response) {
-        var res = response.data;
+//     //Sending Payload to the server
+//     axios(config)
+//       .then(function (response) {
+//         var res = response.data;
 
-        if (res.err_Code) {
-          //Error Message
-          setMessage(res.description);
-        } else {
-          //Import Jasonwebtoken
-          var jwt = require("jsonwebtoken");
+//         if (res.err_Code) {
+//           //Error Message
+//           setMessage(res.description);
+//         } else {
+//           //Import Jasonwebtoken
+//           var jwt = require("jsonwebtoken");
 
-          //Removing Bearer
-          var decoded = res.token.replace("Bearer ", "");
+//           //Removing Bearer
+//           var decoded = res.token.replace("Bearer ", "");
 
-          //Decoding the jwt to get the user data
-          var ud = jwt.decode(decoded);
+//           //Decoding the jwt to get the user data
+//           var ud = jwt.decode(decoded);
 
-          //Taking out of the payload
-          var userId = ud.id;
-          var UserName = ud.username;
-          var Role = ud.role;
+//           //Taking out of the payload
+//           var userId = ud.id;
+//           var UserName = ud.username;
+//           var Role = ud.role;
 
-          //Puting stuff in to the user
-          var user = { UserName: UserName, Role: Role, userId: userId };
+//           //Puting stuff in to the user
+//           var user = { UserName: UserName, Role: Role, userId: userId };
 
-          //Locally storing user Data
-          localStorage.setItem("user_data", JSON.stringify(user));
+//           //Locally storing user Data
+//           localStorage.setItem("user_data", JSON.stringify(user));
 
-          //Go to the user Window
-          window.location.href = "/user";
-        }
-      })
-      .catch(function (error) {
-        //Error function to show error as consol logs
-        setMessage(" " + error);
-      });
-  };
+//           //Go to the user Window
+//           window.location.href = "/user";
+//         }
+//       })
+//       .catch(function (error) {
+//         //Error function to show error as consol logs
+//         setMessage(" " + error);
+//       });
+//   };
 
   return (
     <>
@@ -96,9 +96,10 @@ const SignIn = () => {
 
               <FormLabel> {message} </FormLabel>
 
-              <FormButton type="submit" class="button" onClick={doLogin}>
-                Continue
-              </FormButton>
+//               <FormButton type="submit" class="button" onClick={doLogin}>
+//                 Continue
+//               </FormButton>
+              <FormButton type="submit">Continue</FormButton>
               <p classname="forgot-password text-right">
                 <Link to={"/resetpassword"}>Forgot password?</Link>
               </p>
