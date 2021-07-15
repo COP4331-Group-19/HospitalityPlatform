2aec5160f75ededd07cd12873ea5a33129285aac
avgalbo <54754311+avgalbo@users.noreply.github.com>
2021-07-15 17:24:56 -0400

    Add files via upload

---
+++ src/pages/editaccountguest.js
@@ -0,0 +1,14 @@
+import React from "react";
+import EditAccountGuest from "../components/Guest/EditAccountGuest";
+import ScrollToTop from "../components/ScrollToTop";
+
+function EditAccountGuestPage() {
+  return (
+    <>
+      <ScrollToTop />
+      <EditAccountGuest />
+    </>
+  );
+}
+
+export default EditAccountGuestPage;
