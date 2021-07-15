8bb61e2c0f731883cc00a32e2538bbe09f24fe7b
aygarcia <65561606+aygarcia@users.noreply.github.com>
2021-07-15 18:30:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import Home from "./pages";
 import SigninPage from "./pages/signin";
 import AdminPage from "./pages/admin";
 import EmployeePage from "./pages/employee";
-import GuestPage from './pages/guest';
-import AboutGuestPage from './pages/aboutguest';
-import MyProfileGuestPage from './pages/myprofileguest';
-import PasswordResetPage from './pages/resetpassword';
+import GuestPage from "./pages/guest";
+import AboutGuestPage from "./pages/aboutguest";
+import MyProfileGuestPage from "./pages/myprofileguest";
+import PasswordResetPage from "./pages/resetpassword";
 import EditAccountGuestPage from "./pages/editaccountguest";
+import EditAccountEmployeePage from "./pages/editaccountemployee.js";
+import AboutEmployeePage from "./pages/aboutemployee";
+import OrdersPage from "./pages/orders";
+import MyProfileEmployeePage from "./pages/myprofileemployee";
 
 function App() {
   return (
@@ -20,13 +24,26 @@ function App() {
         <Route path="/signin" component={SigninPage} exact />
         <Route path="/admin" component={AdminPage} exact />
         <Route path="/employee" component={EmployeePage} exact />
-        <Route path='/resetpassword' component={PasswordResetPage} exact />
+        <Route path="/resetpassword" component={PasswordResetPage} exact />
         <Route path="/guest" component={GuestPage} exact />
         <Route path="/aboutguest" component={AboutGuestPage} exact />
+        <Route path="/aboutemployee" component={AboutEmployeePage} exact />
+
         <Route path="/myprofileguest" component={MyProfileGuestPage} exact />
-          <Route
+        <Route path="/orders" component={OrdersPage} exact />
+        <Route
+          path="/myprofileemployee"
+          component={MyProfileEmployeePage}
+          exact
+        />
+        <Route
           path="/editaccountguest"
           component={EditAccountGuestPage}
+          exact
+        />
+        <Route
+          path="/editaccountemployee"
+          component={EditAccountEmployeePage}
           exact
         />
       </Switch>
