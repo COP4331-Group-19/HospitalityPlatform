391e863b68302baf75127ed78e53d5710bf216c2
KartanRaik <84426711+KartanRaik@users.noreply.github.com>
2021-07-15 07:45:52 -0400

    Add files via upload

---
+++ MobileScreens/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+
+//import Screens
+import LoginScreen from './screens/LoginScreen';
+import EmployeeScreen from './screens/EmployeeScreen.js';
+import PasswordChange from './screens/PasswordChange';
+
+//The app function on react
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+//App navigator to all the screens
+const AppNavigator = createStackNavigator({
+  Login: {
+    screen: LoginScreen
+  },
+  Employee: {
+    screen: EmployeeScreen
+  },
+  PasswordChange:{
+    screen: PasswordChange
+  }
+
+},{
+  initialRouteName: "PasswordChange"
+});
+
+//Basically the App
+const AppContainer = createAppContainer(AppNavigator);
+
+//CSS part
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- MobileScreens/PasswordChange.js
+++ MobileScreens/PasswordChange.js
@@ -1,22 +1,16 @@
 import React, { Component, useState } from 'react';
 import { StyleSheet, Text, View, ImageBackground, Button, TextInput, TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons';
-import bp from '../Path.js';
-import jwt from 'react-native-pure-jwt';
 
-function LoginScreen() {
 
-    var Login;
-    var Password;
+function PasswordChange() {
+
+    var Email;
 
     const [message, setMessage] = useState('');
 
-    const doLogin = async event => {
-        setMessage('Logging In');
-    }
-
     const doPassReset = async event => {
-        setMessage('Reseting Password');
+        setMessage('PasswordReset');
     }
 
     return (
@@ -32,54 +26,36 @@ function LoginScreen() {
                     </View>
                     {/*Break*/}
                     <Text>{'\n'}</Text>
-                    {/* UserName */}
-                    <View style={styles.login_pack}>
-                        <Feather name='user' size={40} color="black" />
+
+                    {/* Email */}
+                    <View style={styles.email_pack}>
+                        <Feather name='mail' size={30} color="black" />
                         <TextInput
-                            style={styles.login}
-                            placeholder="UserName"
-                            onChangeText={(val) => { Login = val }}
+                            style={styles.email}
+                            placeholder="Enter Your Email"
+                            onChangeText={(val) => { Email = val }}
                         />
                     </View>
                     {/*Break*/}
                     <Text>{'\n'}</Text>
-                    {/* Password */}
-                    <View style={styles.login_pack}>
-                        <Feather name='eye-off' size={40} color="black" />
-                        <TextInput
-                            style={styles.password}
-                            placeholder="Password"
-                            secureTextEntry={true}
-                            onChangeText={(val) => { Password = val }}
-                        />
-                    </View>
-                    {/*Break*/}
-                    <Text>{'\n'}</Text>
+
+                    {/* Status */}
                     <View>
                         <Text style={styles.status}>{message}</Text>
                     </View>
                     {/*Break*/}
                     <Text>{'\n'}</Text>
+
+                    {/* Reset */}
                     <TouchableOpacity
                         color="black"
                         title="LOGIN"
                         style={styles.button}
-                        onPress={doLogin}
-                    >
-                        <View style={styles.button_pack}>
-                            <Text style={styles.button}>Log In </Text>
-                            <Feather name="log-in" size={40} color="white" />
-                        </View>
-                    </TouchableOpacity>
-                    {/*Break*/}
-                    <Text>{'\n'}</Text>
-                    <TouchableOpacity
-                        color="black"
-                        title="PasswordRes"
-                        style={styles.button}
                         onPress={doPassReset}
                     >
-                    <Text style={styles.resetbutton}>Forgot Password ?</Text>
+                        <View style={styles.button_pack}>
+                            <Text style={styles.button}>Reset</Text>
+                        </View>
                     </TouchableOpacity>
                 </View>
             </ImageBackground>
@@ -102,32 +78,18 @@ const styles = StyleSheet.create({
         alignItems: "center",
         opacity: 0.7
     },
-    login_index: {
-        textAlign: 'center',
-        fontSize: 25,
-        color: 'green',
-        backgroundColor: "orange",
-        fontWeight: 'bold',
-        borderRadius: 5
-    },
-    login: {
+    email: {
         textAlign: 'left',
-        fontSize: 40,
+        fontSize: 20,
         backgroundColor: "lightgrey",
         borderRadius: 5,
-        height: 50
-    },
-    password: {
-        textAlign: 'left',
-        fontSize: 40,
-        backgroundColor: "lightgrey",
-        borderRadius: 5,
-        height: 50
+        height: 50,
+        maxWidth:'80%'
     },
     button: {
         textAlign: 'center',
         fontSize: 30,
-        backgroundColor: 'green',
+        backgroundColor: 'blue',
         borderRadius: 5,
         fontWeight: 'bold',
         height: 40,
@@ -145,8 +107,9 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         width: '75%'
     },
-    login_pack: {
-        justifyContent: 'center',
+    email_pack: {
+        justifyContent: 'flex-start',
+        alignItems:'center',
         flexDirection: 'row',
         backgroundColor: 'lightgrey',
         borderRadius: 5
@@ -154,23 +117,14 @@ const styles = StyleSheet.create({
     button_pack: {
         justifyContent: 'center',
         flexDirection: 'row',
-        backgroundColor: 'green',
+        backgroundColor: 'blue',
         borderRadius: 5
     },
     title: {
         fontSize: 35,
         fontWeight: 'bold',
         color: 'black'
-    },
-    resetbutton:{
-        textAlign: 'center',
-        fontSize: 25,
-        backgroundColor: 'red',
-        borderRadius: 5,
-        fontWeight: 'bold',
-        height: 40,
-        color: 'white'
     }
 });
 
-export default LoginScreen;
+export default PasswordChange;
