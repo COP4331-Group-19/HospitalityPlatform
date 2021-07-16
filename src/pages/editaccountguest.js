4e23853fce2a9bfdf59049ddbe60adc332608e0d
avgalbo <54754311+avgalbo@users.noreply.github.com>
2021-07-15 22:03:44 -0400

    Update editaccountguest.js

--- src/pages/editaccountguest.js
+++ src/pages/editaccountguest.js
@@ -1,12 +1,16 @@
 import React from "react";
 import EditAccountGuest from "../components/Guest/EditAccountGuest";
+import NavbarGuest from "../components/Navbar/NavbarGuest";
 import ScrollToTop from "../components/ScrollToTop";
+import Footer from "../components/Footer";
 
 function EditAccountGuestPage() {
   return (
     <>
+      <NavbarGuest />
       <ScrollToTop />
       <EditAccountGuest />
+      <Footer />
     </>
   );
 }
