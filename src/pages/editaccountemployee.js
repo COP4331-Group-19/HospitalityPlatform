a208d12cb6bf2734e44eb86269d70ab14375a20f
avgalbo <54754311+avgalbo@users.noreply.github.com>
2021-07-15 22:03:07 -0400

    Update editaccountemployee.js

--- src/pages/editaccountemployee.js
+++ src/pages/editaccountemployee.js
@@ -1,12 +1,16 @@
 import React from "react";
 import EditAccountEmployee from "../components/Employee/EditAccountEmployee";
+import NavbarEmployee from "../components/Navbar/NavbarEmployee";
 import ScrollToTop from "../components/ScrollToTop";
+import Footer from "../components/Footer";
 
 function EditAccountEmployeePage() {
   return (
     <>
+      <NavbarEmployee />
       <ScrollToTop />
       <EditAccountEmployee />
+      <Footer />
     </>
   );
 }
