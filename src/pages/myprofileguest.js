7faf978d3ecc09c7458298bf03a1233807ed7c0a
avgalbo <54754311+avgalbo@users.noreply.github.com>
2021-07-16 11:43:28 -0400

    Update myprofileguest.js

--- src/pages/myprofileguest.js
+++ src/pages/myprofileguest.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import MyProfileGuest from "../components/Guest/MyProfileGuest";
 import ScrollToTop from "../components/ScrollToTop";
 import NavbarGuest from "../components/Navbar/NavbarGuest";
+import SidebarGuest from "../components/Sidebar/SidebarGuest.js";
 import Footer from "../components/Footer";
 
 function MyProfileGuestPage() {
@@ -12,7 +13,8 @@ function MyProfileGuestPage() {
   };
   return (
     <>
-      <NavbarGuest />
+      <NavbarGuest toggle={toggle} />
+      <SidebarGuest isOpen={isOpen} toggle={toggle} />
       <ScrollToTop />
       <MyProfileGuest />
       <Footer />
